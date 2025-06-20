72fe38e193db05e81c43a31dcfc6f71a362b2b04
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-06-20 21:39:26 +0200

    feat: added initial database structure(migrations) and models

---
+++ database/migrations/2025_06_20_192355_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('path')->nullable();
+            $table->nestedSet();
+            $table->boolean('is_folder');
+            $table->string('mime');
+            $table->integer('size');
+            $table->timestamps();
+            $table->foreignIdFor(User::class, 'created_by');
+            $table->foreignIdFor(User::class, 'updated_by');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
