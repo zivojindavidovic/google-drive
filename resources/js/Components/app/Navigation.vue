ebfc816beafdc41a84ac34c2902e60ec5c8adcbe
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-06-21 21:53:53 +0200

    feat: changed default scaffolding for auth user

---
+++ resources/js/Components/app/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="min-w-[200px]">
+        <div class="h-[80px] px-3 items-center gap-3">
+            <Link href="/">
+                <ApplicationLogo
+                    class="block h-9 w-auto fill-current text-gray-800"
+                />
+            </Link>
+            GoogleDrive
+        </div>
+        <div class="px-3">
+            <CreateNewDropdown />
+
+            <div class="py-3">
+                <NavLink :active="true" href="/">My Files</NavLink>
+                <NavLink href="/">Shared with me</NavLink>
+                <NavLink href="/">Shared by me</NavLink>
+                <NavLink href="/">Trash</NavLink>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from '@inertiajs/vue3';
+import CreateNewDropdown from "@/Components/app/CreateNewDropdown.vue";
+import NavLink from "@/Components/NavLink.vue";
+
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,11 +1,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Dashboard" />
-
     <AuthenticatedLayout>
         <template #header>
             <h2
